8b6ed5c0fd5bc16c61007e227e096697844e82fb
naveenduraik <naveendurai.k@gmail.com>
2022-10-13 10:25:13 +0530

    repository updated

--- src/main/java/com/phonedirectory/app/repository/ContactRepository.java
+++ src/main/java/com/phonedirectory/app/repository/ContactRepository.java
@@ -7,9 +7,10 @@ import com.phonedirectory.app.model.Contact;
 
 public interface ContactRepository extends CrudRepository<Contact,Integer>  {
 	
-	 @Query(value = "SELECT c.primary_mobile_number FROM pdaboot.user_details ud inner join pdaboot.contact c on ud.user_id=c.user_id where ud.user_id=:userId",nativeQuery = true)
+	 @Query(value = "SELECT ud.username,c.primary_mobile_number FROM pdaboot.user_details ud inner join pdaboot.contact c on ud.user_id=c.user_id where ud.user_id=:userId",nativeQuery = true)
 	   public String userDetailsPhone(@RequestParam("userId") int userId);
 	 
-	 @Query(value = "SELECT c.primary_mobile_number FROM pdaboot.user_details ud inner join pdaboot.contact c on ud.user_id=c.user_id where ud.username=:username",nativeQuery = true)
+	 @Query(value = "SELECT ud.username,c.primary_mobile_number FROM pdaboot.user_details ud inner join pdaboot.contact c on ud.user_id=c.user_id where ud.username=:username",nativeQuery = true)
 	   public String usernameDetailsPhone(@RequestParam("username") String username);
 }
+
