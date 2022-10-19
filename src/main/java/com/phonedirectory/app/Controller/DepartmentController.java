a82a094486ee3310142886f5cf427bae9da45100
naveenduraik <naveendurai.k@gmail.com>
2022-10-19 14:45:55 +0530

    Annotate mapping updated

--- src/main/java/com/phonedirectory/app/Controller/DepartmentController.java
+++ src/main/java/com/phonedirectory/app/Controller/DepartmentController.java
@@ -4,7 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,17 +23,20 @@ public class DepartmentController {
         @Autowired
         private DepartmentService departmentService;
 
-        @RequestMapping(method = RequestMethod.GET,value ="/departments")
+        @GetMapping("/departments")
+        //@RequestMapping(method = RequestMethod.GET,value ="/departments")
         public List<Department> getAllDepartments(){
             return departmentService.getAllDepartments();
         }
         
-        @RequestMapping(method = RequestMethod.GET,value ="/departments/{departmentId}")
+        @GetMapping("/departments/{departmentId")
+       // @RequestMapping(method = RequestMethod.GET,value ="/departments/{departmentId}")
         public Optional<Department> getDepartment(@PathVariable String departmentId){
             return departmentService .getDepartment(departmentId);
         }
     
-        @RequestMapping(method = RequestMethod.POST,value = "/departments")
+        @PostMapping("/departments")
+       // @RequestMapping(method = RequestMethod.POST,value = "/departments")
         public void addContact(@RequestBody Department department){
                  departmentService.addDepartment(department);
         }
@@ -37,12 +44,14 @@ public class DepartmentController {
         //put or patch can be used to update the record -- but patch is best suited to update a particular field
         //in a selected record -- if we are updating the whole record then put can be used 
         //patch responds back at a lesser time line (similar to replacing a bail in cricket kit rather than replacing the whole kit bag)
-        @RequestMapping(method = RequestMethod.PATCH,value="/departments/{departmentId}")
+        @PatchMapping("/departments/{departmentId}")
+        //@RequestMapping(method = RequestMethod.PATCH,value="/departments/{departmentId}")
         public void updateContact(@RequestBody Department department, @PathVariable String departmentId){
             departmentService.updateDepartment(department,departmentId );
         }
     
-        @RequestMapping(method = RequestMethod.DELETE,value ="/departments/{departmentId}")
+        @DeleteMapping("/departments/{departmentId}")
+       // @RequestMapping(method = RequestMethod.DELETE,value ="/departments/{departmentId}")
         public void deleteContact(@PathVariable String departmentId){
             departmentService.deleteDepartment(departmentId);
         }      
