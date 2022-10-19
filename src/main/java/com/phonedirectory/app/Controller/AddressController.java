9efeee42b15f577f6c44b8fb5a8b7679e8b9da1f
naveenduraik <naveendurai.k@gmail.com>
2022-10-19 19:18:28 +0530

    unused import removed

--- src/main/java/com/phonedirectory/app/Controller/AddressController.java
+++ src/main/java/com/phonedirectory/app/Controller/AddressController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.phonedirectory.app.model.Address;
