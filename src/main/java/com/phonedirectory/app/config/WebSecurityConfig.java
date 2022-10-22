0b2df53228b4f01936575aa2782976b9d9e79ad7
naveenduraik <naveendurai.k@gmail.com>
2022-10-22 18:00:25 +0530

    comment removed

--- src/main/java/com/phonedirectory/app/config/WebSecurityConfig.java
+++ src/main/java/com/phonedirectory/app/config/WebSecurityConfig.java
@@ -21,8 +21,6 @@ import com.phonedirectory.app.service.JWTUserDetailsService;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	/*  @Autowired
-	private JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint; */
  
 	@Autowired
 	private JWTUserDetailsService jwtUserDetailsService;
@@ -52,26 +50,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-			// We don't need CSRF for this example		
-		/* httpSecurity.csrf().disable().
-				// dont authenticate this particular request
-				authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
-				// all other requests need to be authenticated
-				anyRequest().authenticated().and().
-				// make sure we use stateless session; session won't be used to
-				// store user's state.
-				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
- 
-		// Add a filter to validate the tokens with every request
-		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);*/
-	
-		httpSecurity
-    .antMatcher("/api/**")
-    .authorizeRequests() //
-        .anyRequest().authenticated() //
-        .and()
-    .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+				httpSecurity.antMatcher("/api/**")
+    						.authorizeRequests() //
+        					.anyRequest().authenticated() //
+        					.and()
+    						.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
 	}
 
