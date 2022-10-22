81c998567dec57b562fd7b309d53525a8f4b6262
naveenduraik <naveendurai.k@gmail.com>
2022-10-22 22:27:00 +0530

    unused imports removed

--- src/main/java/com/phonedirectory/app/repository/UserDetailsRepository.java
+++ src/main/java/com/phonedirectory/app/repository/UserDetailsRepository.java
@@ -1,8 +1,6 @@
 package com.phonedirectory.app.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.phonedirectory.app.model.UserDetails;
 
