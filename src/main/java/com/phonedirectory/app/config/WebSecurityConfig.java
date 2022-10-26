63a445b499ca750507d3a31e7e8aaaad07d3529f
naveenduraik <naveendurai.k@gmail.com>
2022-10-26 13:05:25 +0530

    csrf updated

--- src/main/java/com/phonedirectory/app/config/WebSecurityConfig.java
+++ src/main/java/com/phonedirectory/app/config/WebSecurityConfig.java
@@ -68,7 +68,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// dont authenticate this particular request
 		try {
 		
-			httpSecurity.csrf().and().authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
+			httpSecurity.csrf().disable().authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
 					// all other requests need to be authenticated
 					anyRequest().authenticated().and().
 					// make sure we use stateless session; session won't be used to
